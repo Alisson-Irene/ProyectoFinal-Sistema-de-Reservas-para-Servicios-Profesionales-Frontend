8b8df53087940ee5ba572f268278e8bb98147a96
Jose <ga0651032023@unab.edu.sv>
2026-04-01 20:44:57 -0600

    feat:Continuacion del frontend

--- reservas-frontend/src/app/app.ts
+++ reservas-frontend/src/app/app.ts
@@ -1,12 +1,51 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('reservas-frontend');
+
+  api = 'http://localhost:3000/api';
+
+  constructor(private http: HttpClient) {}
+
+  mensaje = '';
+
+  servicio = {
+    nombre: '',
+    descripcion: '',
+    precio: 0
+  };
+
+  servicios: any[] = [];
+
+  crearServicio() {
+    this.http.post(`${this.api}/servicios`, this.servicio).subscribe({
+      next: () => {
+        this.mensaje = 'Servicio creado';
+        this.servicio = { nombre: '', descripcion: '', precio: 0 };
+        this.cargarServicios();
+      },
+      error: () => {
+        this.mensaje = 'Error al crear servicio';
+      }
+    });
+  }
+
+  cargarServicios() {
+    this.http.get<any[]>(`${this.api}/servicios`).subscribe(res => {
+      this.servicios = res;
+    });
+  }
+
+  ngOnInit() {
+    this.cargarServicios();
+  }
 }
