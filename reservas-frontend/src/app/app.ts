3e6a4a10f90dfdd047c8b613ab655f46f66ab57b
Alexandra Yamileth <sg0399032023@unab.edu.sv>
2026-04-11 18:36:03 -0600

    Feat: Agregando codigo

--- reservas-frontend/src/app/app.ts
+++ reservas-frontend/src/app/app.ts
@@ -11,7 +11,6 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.css'
 })
 export class App {
-
   api = 'http://localhost:3000/api';
 
   constructor(private http: HttpClient) {}
@@ -25,32 +24,84 @@ export class App {
   };
 
   servicios: any[] = [];
+  editando = false;
+  servicioEditandoId: number | null = null;
 
   crearServicio() {
     this.http.post(`${this.api}/servicios`, this.servicio).subscribe({
       next: () => {
         this.mensaje = 'Servicio creado';
-        this.servicio = { nombre: '', descripcion: '', precio: 0 };
+        this.limpiarServicio();
+        this.cargarServicios();
       },
-      error: () => {
-        this.mensaje = 'Error al crear servicio';
+      error: (err: any) => {
+        console.log(err);
+        this.mensaje = err?.error?.message || 'Error al crear servicio';
       }
     });
   }
 
   cargarServicios() {
     this.http.get<any[]>(`${this.api}/servicios`).subscribe({
-      next: (data) => {
-        this.servicios = data;
-        this.mensaje = '';
-        console.log('Servicios cargados:', data);
+      next: (res: any[]) => {
+        this.servicios = res;
       },
-      error: (error) => {
-        console.error('Error al cargar servicios:', error);
-        this.mensaje = 'Error al cargar servicios';
+      error: (err: any) => {
+        console.log(err);
       }
     });
-  } 
+  }
 
- 
+  editarServicio(servicio: any) {
+    this.editando = true;
+    this.servicioEditandoId = servicio.id;
+    this.servicio = {
+      nombre: servicio.nombre,
+      descripcion: servicio.descripcion,
+      precio: servicio.precio
+    };
+  }
+
+  actualizarServicio() {
+    if (!this.servicioEditandoId) return;
+
+    this.http.put(`${this.api}/servicios/${this.servicioEditandoId}`, this.servicio).subscribe({
+      next: () => {
+        this.mensaje = 'Servicio actualizado';
+        this.limpiarServicio();
+        this.cargarServicios();
+      },
+      error: (err: any) => {
+        console.log(err);
+        this.mensaje = err?.error?.message || 'Error al actualizar servicio';
+      }
+    });
+  }
+
+  cancelarEdicionServicio() {
+    this.limpiarServicio();
+  }
+
+  limpiarServicio() {
+    this.servicio = { nombre: '', descripcion: '', precio: 0 };
+    this.editando = false;
+    this.servicioEditandoId = null;
+  }
+
+  eliminarServicio(id: number) {
+  this.http.delete(`${this.api}/servicios/${id}`).subscribe({
+    next: () => {
+      this.mensaje = 'Servicio eliminado';
+      this.cargarServicios();
+    },
+    error: (err: any) => {
+      console.log(err);
+      this.mensaje = err?.error?.message || 'Error al eliminar servicio';
+    }
+  });
+}
+
+  ngOnInit() {
+    this.cargarServicios();
+  }
 }
